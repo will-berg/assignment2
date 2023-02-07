b065817c246333d4a73dd0c828a5cdb594fd8684
William Berg <wm.bg@outlook.com>
2023-02-07 11:47:20 +0100

    feat: #17 function to run build script and minor template additions

--- ci_server.py
+++ ci_server.py
@@ -1,8 +1,38 @@
 from flask import Flask, request
 import json
+import sys
+import subprocess
 
 app = Flask(__name__)
 
+# Runs on every subscribed event
 @app.route("/", methods=["POST"])
 def handle():
-	print(request.data)
+	run_build()
+	static_analysis(request.data)
+	run_tests(request.data)
+
+
+# Run build script
+def run_build():
+	res = subprocess.run(["bash", "build.sh"])
+	if res.returncode == 0:
+		return True
+	else:
+		return False
+
+
+# The CI server performs static analysis on the updated branch
+def static_analysis(req):
+	pass
+
+
+# The CI server executes the test suite on the branch that was changed
+def run_tests(req):
+	pass
+
+
+# The CI server sets commit status
+def notify(req, status):
+	pass
+
