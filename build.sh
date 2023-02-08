97d251adadbaced945ea8a1198464c02a6c5fff3
Aron Bergman <bathingrad@gmail.com>
2023-02-08 12:40:56 +0100

    fix: Create dummy token file if it doesn't exist

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ python3 -m venv venv
 . venv/bin/activate
 pip install gunicorn
 pip install -r requirements.txt
+[ ! -f github_token.py ] && cat <<EOF > github_token.py
+github_token = "your_token_here"
+EOF
 deactivate
