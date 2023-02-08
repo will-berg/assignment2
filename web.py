7d58d3b9cda86ebc08578bc270458af5c5a051e6
ElHachem02 <peterelhachem02@gmail.com>
2023-02-08 21:50:56 +0100

    feat: resolves #84 list the builds in a webpage

--- web.py
+++ web.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+# create anoter route for displaying files
+@app.route('/')
+def listBuilds():
+	html_path = os.path.join(os.path.dirname(__file__), 'templates', 'fileList.html')
+    # return the string that I want to send back to the browser
+	directory = '/srv/ci'
+	files = os.listdir(directory)
+	return render_template('fileList.html', files=files)
+
 
 @app.route("/<id>")
 def show_build(id):
@@ -11,4 +21,4 @@ def show_build(id):
 
 	with open(f"/srv/ci/{id}", "r") as f:
 		contents = f.read()
-	return (contents, 200, headers)
+	return (contents, 200, headers)	
