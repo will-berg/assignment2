f7a96ee0b8c4c4c3914ca2902d2726e0232d7f56
Aron Bergman <bathingrad@gmail.com>
2023-02-08 19:19:23 +0100

    test: Activate the virtual environment if it isn't already

--- lint.sh
+++ lint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[[ -z $VIRTUAL_ENV ]] && . venv/bin/activate
 pip install pylint
 pylint --errors-only ci_server.py
 pylint --errors-only updater.py
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+[[ -z $VIRTUAL_ENV ]] && . venv/bin/activate
 python3 -m unittest tests/ci_server.py
 python3 -m unittest tests/updater.py
