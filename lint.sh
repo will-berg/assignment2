7aefdb6c9abcdee584a18129840f3c16163bfedc
Aron Bergman <bathingrad@gmail.com>
2023-02-08 20:20:22 +0100

    feat: Create web server to display CI output (#66)
    Resolves #2

--- lint.sh
+++ lint.sh
@@ -2,4 +2,5 @@
 [[ -z $VIRTUAL_ENV ]] && . venv/bin/activate
 pip install pylint
 pylint --errors-only ci_server.py
+pylint --errors-only web.py
 pylint --errors-only updater.py
--- util/reload.sh
+++ util/reload.sh
@@ -2,4 +2,5 @@
 cd $(dirname $0)
 
 [ -f /tmp/pid_updater ] && kill -HUP $(cat /tmp/pid_updater)
+[ -f /tmp/pid_web ] && kill -HUP $(cat /tmp/pid_web)
 [ -f /tmp/pid_ci ] && kill -HUP $(cat /tmp/pid_ci)
